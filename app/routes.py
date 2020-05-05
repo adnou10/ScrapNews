3de72ccefdf913a8d3af81f79f50610f3b3fcfa8
Nouhaila Adil <adil.nouhaila@gmail.com>
2020-05-05 18:25:24 +0000

    get url from request

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,20 @@
 #import modules
-from flask import render_template, redirect, flash, url_for, Response
+from flask import render_template, redirect, flash, url_for, Response,request
 from app import app
 from app.forms import URLForm
 from app.models.article import Article
 
-
 #Home page that shows the 'get url' form
 @app.route('/')
-@app.route('/index',methods=['GET','POST'])
+@app.route('/index')
 def index():
     form = URLForm()
-    if form.validate_on_submit():               # if "post"
-        url=form.url.data                       # get url
-        flash('URL entered {}'.format(url))
-        return redirect(url_for('articles'),url=url)
     return render_template('index.html', form=form)
 
-@app.route('/articles/<url>')#,methods=['GET','POST'])
-def articles(url):
+@app.route('/articles',methods=['POST'])
+def articles():
+    if request.method == 'POST':
+        url=request.form.get('url')
     return url
     #articles=Article.objects.to_json()
     #return Response(articles,mimetype="application/json",status=200)

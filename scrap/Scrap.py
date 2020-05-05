53f1028963f560f7bfb6b24c1a77510074374c2e
Nouhaila Adil <adil.nouhaila@gmail.com>
2020-05-05 01:16:34 +0000

    use request instead urllib.request and manage None type errors

--- scrap/Scrap.py
+++ scrap/Scrap.py
@@ -7,13 +7,15 @@ Created on Mon May  4 22:38:42 2020
 
 # import libraries
 from bs4 import BeautifulSoup
-import urllib.request
+from requests import get
 
 
 # get html page    
 def getHTMLPage(link):
-    page = urllib.request.urlopen(link)                         # query the website and return the html to the variable 'page'
-    soup = BeautifulSoup(page, 'html.parser')                       # parse the html using beautiful soup and store in variable 'soup'
+    print('link',link)
+    page=get(link)
+    #page = urllib.request.urlopen(link).read()                         # query the website and return the html to the variable 'page'
+    soup = BeautifulSoup(page.text, 'html.parser')                       # parse the html using beautiful soup and store in variable 'soup'
     return soup
 
 class ScrapPage(object):
@@ -38,34 +40,42 @@ class ScrapArticles(object):
     
     # get the composants of the article
     def run(self,article,link): 
-        self.url = link + article.find('a', attrs={'class': 'media__link'}).get('href')   # url of an article from href attribute
+        self.url =  article.find('a', attrs={'class': 'media__link'}).get('href')   # url of an article from href attribute
+        if "https://www" not in self.url:
+            self.url = link + self.url                                      
         summ = article.find('p', attrs={'class': 'media__summary'})               # article's summary
         if summ!= None:
              self.summary=summ.text
         soup=getHTMLPage(self.url)
-        body = soup.find('div', attrs={'class': 'story-body'})
-        self.headline= body.find('h1', attrs={'class': 'story-body__h1'}).text            # get the headline of the article
-        authors_txt = body.find('span', attrs={'class': 'byline__name'})           # get the authors of the article
-        if authors_txt!= None:
-            authors_txt = body.find('span', attrs={'class': 'byline__name'}).text  
-            names = authors_txt.split('By ')
-            names.remove('')
-            self.authors = names
-        content=body.find('div', attrs={'property': 'articleBody'})                  # get the content of the article
-        for i in content.find_all(['script','figure','div']):                        # Clean the content from superflu
-               i.decompose()
-        self.content=content.text
-        tags=soup.find('ul', attrs={'class': 'tags-list'}).find_all('li')
-        for i in tags:
-            keyword=i.text
-            self.keywords.append(keyword)
+        if soup!=None:
+            body = soup.find('div', attrs={'class': 'story-body'})
+            if body != None:
+                title=body.find('h1', attrs={'class': 'story-body__h1'})
+                if title != None:
+                    self.headline= title .text            # get the headline of the article
+                authors_txt = body.find('span', attrs={'class': 'byline__name'})           # get the authors of the article
+                if authors_txt!= None:
+                    authors_txt = body.find('span', attrs={'class': 'byline__name'}).text  
+                    names = authors_txt.split('By ')
+                    names.remove('')
+                    self.authors = names
+                    content=body.find('div', attrs={'property': 'articleBody'})                  # get the content of the article
+                    for i in content.find_all(['script','figure','div']):                        # Clean the content from superflu
+                        i.decompose()
+                        self.content=content.text
+            tag=soup.find('ul', attrs={'class': 'tags-list'})
+            if tag != None:
+                tags=tag.find_all('li')
+                for i in tags:
+                    keyword=i.text
+                    self.keywords.append(keyword)
 
         
 
 
 
 
-"""link='https://www.bbc.com'       
+'''link='https://www.bbc.com'       
 a=ScrapPage(link)        
 articles=a.Articles()    
 for article in articles[0:4]:
@@ -76,5 +86,5 @@ for article in articles[0:4]:
     print(b.content)
     print(b.url)
     print(b.summary)
-    print(b.keywords)
-"""
+    print(b.keywords)'''
+

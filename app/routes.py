665fc768f2fad3c556372597e0d0aa12cb726afa
Nouhaila Adil <adil.nouhaila@gmail.com>
2020-05-11 16:07:44 +0000

    update routes

--- app/routes.py
+++ app/routes.py
@@ -66,7 +66,7 @@ def result(job_key):
 @app.route('/all')
 @app.route('/articles')
 def articles():
-    art=Article.objects
+    art=Article.objects()
     return  render_template('articles.html',arts=arts)   #display articles in db
     
     #return redirect(url_for('index'))
